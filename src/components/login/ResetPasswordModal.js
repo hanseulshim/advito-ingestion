8a5d9dfe2061182fce60988b60ddeb38597b6daa
Hanseul Shim <hanseulshim@gmail.com>
2020-02-21 03:20:27 -0500

    Add error handling and switch to apollo/client

--- src/components/login/ResetPasswordModal.js
+++ src/components/login/ResetPasswordModal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useMutation } from '@apollo/react-hooks'
+import { useMutation } from '@apollo/client'
 import { Modal, Form, Input, Button } from 'antd'
 import { SEND_RESET_PASSWORD } from 'api'
 import ErrorMessage from 'components/common/ErrorMessage'
