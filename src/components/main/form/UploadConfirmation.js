a0107cce07f43eb84d4a4352addc5a789172a7d8
joneil@boostlabs.com <joneil@boostlabs.com>
2020-03-02 21:39:05 -0600

    working parse

--- src/components/main/form/UploadConfirmation.js
+++ src/components/main/form/UploadConfirmation.js
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { SpinLoader } from 'components/common/Loader'
 import ErrorMessage from 'components/common/ErrorMessage'
 import { Modal } from 'antd'
 import { CLIENT_LIST } from 'api/queries'
+import XLSX from 'xlsx'
 
 const UploadConfirmation = ({ visible, file, ...props }) => {
   const { loading, error, data } = useQuery(CLIENT_LIST)
-  if (loading) return <SpinLoader />
-  if (error) return <ErrorMessage error={error} />
+  const [rowCount, setRowCount] = useState([])
+
+  useEffect(() => {
+    async function getFileData() {
+      if (file) {
+        const data = await parseExcel(file)
+        setRowCount(data)
+      }
+    }
+    getFileData()
+  }, [file])
 
   const getClientName = clientId => {
     return (
@@ -16,6 +26,27 @@ const UploadConfirmation = ({ visible, file, ...props }) => {
     )
   }
 
+  const parseExcel = file =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.readAsBinaryString(file)
+      reader.onload = e => {
+        var workbook = XLSX.read(e.target.result, { type: 'binary' })
+        let arr = []
+
+        workbook.SheetNames.forEach(function(sheetName) {
+          var XL_row_object = XLSX.utils.sheet_to_row_object_array(
+            workbook.Sheets[sheetName]
+          )
+          arr = [...arr, ...XL_row_object]
+        })
+        resolve(arr)
+      }
+      reader.onerror = error => reject(error)
+    })
+
+  if (loading) return <SpinLoader />
+  if (error) return <ErrorMessage error={error} />
   return (
     <Modal
       title="Confirm File Upload"
@@ -25,7 +56,8 @@ const UploadConfirmation = ({ visible, file, ...props }) => {
     >
       {file ? (
         <p>
-          You are about to upload <b>{file.name}</b> with X rows for client{' '}
+          You are about to upload <b>{file.name}</b> with{' '}
+          <b>{rowCount.length}</b> rows for client{' '}
           {getClientName(props.selectedClient)}. Do you wish to continue?
         </p>
       ) : (
