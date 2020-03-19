19dffa14fb476853d89ccfcaaff18b4d72782d19
Hanseul Shim <hanseulshim@gmail.com>
2020-03-19 10:13:06 -0400

    Merge branch 'master' of github.com:boostlabscom/advito-ingestion

--- src/helper.js
+++ src/helper.js
@@ -2,56 +2,54 @@ import history from './history'
 import moment from 'moment'
 
 export const formatDate = date => {
-  return date ? moment(date).format('MMMM DD, YYYY') : ''
+	return date ? moment(date).format('MMMM DD, YYYY') : ''
 }
 
 export const getToken = () => {
-  if (localStorage.getItem('advito-ingestion-user')) {
-    const { sessionToken } = JSON.parse(
-      localStorage.getItem('advito-ingestion-user')
-    )
-    return sessionToken
-  } else return ''
+	if (localStorage.getItem('advito-ingestion-user')) {
+		const { sessionToken } = JSON.parse(
+			localStorage.getItem('advito-ingestion-user')
+		)
+		return sessionToken
+	} else return ''
 }
 
 export const getUser = () => {
-  if (localStorage.getItem('advito-ingestion-user')) {
-    const user = JSON.parse(localStorage.getItem('advito-ingestion-user'))
-    return { ...user }
-  } else {
-    return {}
-  }
+	if (localStorage.getItem('advito-ingestion-user')) {
+		const user = JSON.parse(localStorage.getItem('advito-ingestion-user'))
+		return { ...user }
+	} else {
+		return {}
+	}
 }
 
 export const updateUserName = displayName => {
-  if (localStorage.getItem('advito-ingestion-user')) {
-    const user = JSON.parse(localStorage.getItem('advito-ingestion-user'))
-    user.displayName = displayName
-    localStorage.setItem('advito-ingestion-user', JSON.stringify(user))
-  }
+	if (localStorage.getItem('advito-ingestion-user')) {
+		const user = JSON.parse(localStorage.getItem('advito-ingestion-user'))
+		user.displayName = displayName
+		localStorage.setItem('advito-ingestion-user', JSON.stringify(user))
+	}
 }
 
 export const setUser = user => {
-  if (localStorage.getItem('advito-ingestion-user')) {
-    localStorage.removeItem('advito-ingestion-user')
-  }
-  localStorage.setItem('advito-ingestion-user', JSON.stringify(user))
+	if (localStorage.getItem('advito-ingestion-user')) {
+		localStorage.removeItem('advito-ingestion-user')
+	}
+	localStorage.setItem('advito-ingestion-user', JSON.stringify(user))
 }
 
 export const removeUser = () => {
-  localStorage.removeItem('advito-ingestion-user')
-  history.push('/login')
+	localStorage.removeItem('advito-ingestion-user')
+	history.push('/login')
 }
 
 export const getApi = () => {
-  const REACT_APP_STAGE = process.env.REACT_APP_STAGE
-  return REACT_APP_STAGE === 'dev'
-    ? 'https://sp0owoqyr0.execute-api.us-east-2.amazonaws.com/dev/graphql'
-    : REACT_APP_STAGE === 'alpha'
-    ? 'https://8t09b8y0q4.execute-api.us-east-2.amazonaws.com/alpha/graphql'
-    : REACT_APP_STAGE === 'beta'
-    ? 'https://q0x8h2ty95.execute-api.us-east-2.amazonaws.com/beta/graphql'
-    : REACT_APP_STAGE === 'prod'
-    ? 'https://2h33cdp67l.execute-api.us-east-2.amazonaws.com/prod/graphql'
-    : 'http://localhost:4000/graphql'
+	const REACT_APP_STAGE = process.env.REACT_APP_STAGE
+	return REACT_APP_STAGE === 'dev'
+		? 'https://sp0owoqyr0.execute-api.us-east-2.amazonaws.com/dev/graphql'
+		: REACT_APP_STAGE === 'staging'
+		? 'https://0inqpfbuo0.execute-api.us-east-2.amazonaws.com/staging/graphql'
+		: REACT_APP_STAGE === 'prod'
+		? 'https://2h33cdp67l.execute-api.us-east-2.amazonaws.com/prod/graphql'
+		: 'http://localhost:4000/graphql'
 }
