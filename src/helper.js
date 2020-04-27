a56e4ba73f0ba23e57268ed29380b53ce655f56f
Hanseul Shim <hanseulshim@gmail.com>
2020-04-27 09:43:48 -0400

    Default auth server to dev api instead of local

--- src/helper.js
+++ src/helper.js
@@ -3,7 +3,7 @@ import moment from 'moment'
 import isEmpty from 'lodash.isempty'
 import queryString from 'query-string'
 
-export const formatDate = date => {
+export const formatDate = (date) => {
 	return date ? moment(date).format('MMMM DD, YYYY') : ''
 }
 
@@ -29,7 +29,7 @@ export const getUser = () => {
 	}
 }
 
-export const updateUserName = displayName => {
+export const updateUserName = (displayName) => {
 	if (localStorage.getItem('advito-user')) {
 		const user = JSON.parse(localStorage.getItem('advito-user'))
 		user.displayName = displayName
@@ -37,7 +37,7 @@ export const updateUserName = displayName => {
 	}
 }
 
-export const setUser = user => {
+export const setUser = (user) => {
 	if (localStorage.getItem('advito-user')) {
 		localStorage.removeItem('advito-user')
 	}
@@ -69,13 +69,13 @@ export const getApi = () => {
 
 export const getAuthApi = () => {
 	const REACT_APP_STAGE = process.env.REACT_APP_STAGE
-	return REACT_APP_STAGE === 'dev'
-		? 'https://mv3ohvxam8.execute-api.us-east-2.amazonaws.com/dev/graphql'
+	return REACT_APP_STAGE === 'local'
+		? 'http://localhost:8080/local/graphql'
 		: REACT_APP_STAGE === 'alpha'
 		? 'https://mv3ohvxam8.execute-api.us-east-2.amazonaws.com/dev/graphql'
 		: REACT_APP_STAGE === 'beta'
 		? 'https://sewnh0p54a.execute-api.us-east-2.amazonaws.com/staging/graphql'
 		: REACT_APP_STAGE === 'prod'
 		? 'https://lkkkz4lxwc.execute-api.us-east-2.amazonaws.com/production/graphql'
-		: 'http://localhost:8080/local/graphql'
+		: 'https://mv3ohvxam8.execute-api.us-east-2.amazonaws.com/dev/graphql'
 }
